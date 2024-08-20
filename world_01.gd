30ee7f57d0a74be29a304d07c46eb9e0547e5879
Allef R. Schmidt <dev.allefschmidt@gmail.com>
2024-08-20 00:02:49 -0300

    add bubble dialog

--- world_01.gd
+++ world_01.gd
@@ -1,10 +1,20 @@
 extends Node2D
 
-func _input(event: InputEvent):
-	# check if a dialog is already running
-	if Dialogic.current_timeline != null:
-		return
+@onready var player = $player  # Reference to your player node
+@onready var dialogic_character = load("res://timelines/timby.dch")
+@onready var timeline_path = "res://timelines/introduction.dtl"
 
-	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
-		Dialogic.start('introduction')
+func _input(event: InputEvent):
+	# Trigger the dialog only when the "X" key is pressed and no dialog is running
+	if event is InputEventKey and event.keycode == KEY_X and event.pressed:
+		trigger_dialog()
 		get_viewport().set_input_as_handled()
+
+func trigger_dialog():
+	# Check if no other dialog is currently running
+	if Dialogic.current_timeline == null:
+		# Start the dialog timeline with the player as the node the dialog bubble should follow
+		var dialogic_layout = Dialogic.start(timeline_path)
+		dialogic_layout.register_character(dialogic_character, player)
+	else:
+		print("Dialogic timeline is already running.")
