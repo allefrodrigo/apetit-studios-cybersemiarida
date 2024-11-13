435d565783013046b16453c461c821b1a55f3ad5
Allef R. Schmidt <dev.allefschmidt@gmail.com>
2024-11-13 19:06:11 -0300

    arquivos

---
+++ scripts/goal.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@onready var transition: CanvasLayer = $"../transition"
+@export var next_level : String = ""
+
+func _on_body_entered(body):
+	var scene_to_spawn = preload("res://Pickups/Feedback/feedback.tscn")
+	var new_scene_instance = scene_to_spawn.instantiate()
+	get_tree().current_scene.add_child(new_scene_instance)  # Add the instance as a child of the current scene
+	new_scene_instance.global_position = global_position
+	if body.name == "Player" and !next_level == "":
+		transition.change_scene(next_level)
+	else:
+		print("no scene")
