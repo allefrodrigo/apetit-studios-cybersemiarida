73a5037eb020debdeec6bcfe66e023db89880b0d
mijoderato <vagnerbritot@gmail.com>
2025-02-26 11:06:27 -0300

    :add tiled
    adicionado tiled na pasta raiz

---
+++ levels/Level_design/TILED/CSA/Tileset/tileset_cafundo.tsx
@@ -0,0 +1,319 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset_cafundo" tilewidth="16" tileheight="16" tilecount="575" columns="25">
+ <image source="../../../Tilesets/tileset_cafundo.png" width="400" height="368"/>
+ <tile id="26">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="64">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="78">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="79">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="101">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="102">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="107">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="109">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="110">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="114">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="126">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="127">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="128">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="129">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="130">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="132">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="133">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="134">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="135">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="136">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="295">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="297">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="344">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="345">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="347">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="372">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="394">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="403">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="404">
+  <objectgroup>
+   <object id="1" type="Plataforma" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
