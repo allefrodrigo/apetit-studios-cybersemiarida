818c5e87735a995c13c79e02b06712fcf8e9319b
mijoderato <vagnerbritot@gmail.com>
2025-05-28 22:22:43 -0300

    :add ost

---
+++ levels/Level_design/TILED/CSA/desert_tileset_png.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="desert_tileset_png" tilewidth="16" tileheight="16" tilecount="575" columns="25">
+ <image source="../../../../../../apetit-studios-cybersemiarida-assets/apetit-studios-cybersemiarida-assets/Deserto_Tilesets/ground_tileset/ground/_tileset_png.png" width="400" height="368"/>
+</tileset>
